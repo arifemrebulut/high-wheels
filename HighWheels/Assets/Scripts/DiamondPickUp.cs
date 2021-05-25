58cd965c58ba1b1c08b7d329c9951b8878d7eaed
Emre Bulut <80202387+arifemrebulut@users.noreply.github.com>
2021-05-25 17:33:16 +0300

    Refactor Diamond Particle

--- HighWheels/Assets/Scripts/DiamondPickUp.cs
+++ HighWheels/Assets/Scripts/DiamondPickUp.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class DiamondPickUp : MonoBehaviour, ICollideable
 {
+    [SerializeField] GameObject diamondPickUpParticle;
+
     public void ExicuteCollisionActions()
     {
+        PlayPartcleEffects();
+
         EventBroker.CallOnPickUpDiamond();
         Destroy(gameObject);
+    }
+
+    private void PlayPartcleEffects()
+    {
+        Instantiate(diamondPickUpParticle, transform.position, Quaternion.identity);
     }
 }
