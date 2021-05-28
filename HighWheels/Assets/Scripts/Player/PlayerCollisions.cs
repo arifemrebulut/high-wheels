b4dbc67331fc0f7e94fcaa402e63ad8194c350a5
Emre Bulut <80202387+arifemrebulut@users.noreply.github.com>
2021-05-28 21:28:56 +0300

    Fix Player Collision bug with blocks

--- HighWheels/Assets/Scripts/Player/PlayerCollisions.cs
+++ HighWheels/Assets/Scripts/Player/PlayerCollisions.cs
@@ -4,26 +4,57 @@ public class PlayerCollisions : MonoBehaviour
 {
     private bool canCollide = true;
 
-    private void Update()
+    private BoxCollider boxCollider;
+
+    private void Awake()
     {
-        Debug.Log(canCollide);
+        boxCollider = GetComponent<BoxCollider>();
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Block"))
+    //    {
+    //        if (CarSuspensions.currentSuspensionCount == 0 && canCollide)
+    //        {
+    //            EventBroker.CallOnGameOver();
+    //        }
+    //        else if (CarSuspensions.currentSuspensionCount > 0 && canCollide)
+    //        {
+    //            ICollideable collideable = other.gameObject.GetComponent<ICollideable>();
+
+    //            collideable.ExicuteCollisionActions();
+    //            canCollide = false;
+    //        }
+    //    }
+    //    else if (other.gameObject.CompareTag("SuspensionPickUp"))
+    //    {
+    //        Debug.Log("PickSuspension");
+    //        other.gameObject.GetComponent<ICollideable>().ExicuteCollisionActions();
+    //    }
+    //    else if (other.gameObject.CompareTag("DiamondPickUp"))
+    //    {
+    //        other.gameObject.GetComponent<ICollideable>().ExicuteCollisionActions();
+    //    }      
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Block"))
         {
-            if (CarSuspensions.currentSuspensionCount == 0 && canCollide)
+            if (CarSuspensions.currentSuspensionCount == 0)
             {
                 EventBroker.CallOnGameOver();
             }
-            else if (CarSuspensions.currentSuspensionCount > 0 && canCollide)
+            else if (CarSuspensions.currentSuspensionCount > 0)
             {
+                DeactivateCollider();
                 ICollideable collideable = other.gameObject.GetComponent<ICollideable>();
 
                 collideable.ExicuteCollisionActions();
-                canCollide = false;
-                Invoke("ChangeToCanCollide", 1f);
+
+                Invoke("ActivateCollider", 0.8f);
+                
             }
         }
         else if (other.gameObject.CompareTag("SuspensionPickUp"))
@@ -34,15 +65,17 @@ public class PlayerCollisions : MonoBehaviour
         else if (other.gameObject.CompareTag("DiamondPickUp"))
         {
             other.gameObject.GetComponent<ICollideable>().ExicuteCollisionActions();
-        }      
+        }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void ActivateCollider()
     {
-        if (other.gameObject.CompareTag("Block"))
-        {
-            canCollide = true;
-        }
+        boxCollider.enabled = true;
+    }
+
+    private void DeactivateCollider()
+    {
+        boxCollider.enabled = false;
     }
 
     private void ChangeToCanCollide()
